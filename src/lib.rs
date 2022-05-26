544a5015bda0c55ec2a1af0865d037285fc6d5cf
Matthew <matthewblake0625@gmail.com>
2022-05-26 19:16:40 -0400

    changed it so the image output is png.

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,34 @@
+use image::{Rgb, RgbImage};
+use std::fs::write;
+
 #[test]
 fn test() -> std::io::Result<()> {
-    const IMAGE_WIDTH: usize = 256;
-    const IMAGE_HEIGHT: usize = 256;
+    make_image()
+}
 
-    println!("{:?}", (IMAGE_WIDTH, IMAGE_HEIGHT));
+fn make_image() -> std::io::Result<()> {
+    const IMAGE_WIDTH: u32 = 256;
+    const IMAGE_HEIGHT: u32 = 256;
 
-    let mut output: String = String::from("");
+    let mut img = RgbImage::new(IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for i in 0..(IMAGE_WIDTH * IMAGE_HEIGHT) {
         let x = i % IMAGE_WIDTH;
         let y = i / IMAGE_WIDTH;
 
-        let r: f64 = x as f64 / IMAGE_WIDTH as f64;
-        let g: f64 = y as f64 / IMAGE_HEIGHT as f64;
-        let b: f64 = 0.25;
+        let r: f32 = x as f32 / IMAGE_WIDTH as f32;
+        let g: f32 = y as f32 / IMAGE_HEIGHT as f32;
+        let b: f32 = 0.25;
 
-        let ir: i64 = (r * 256.0) as i64;
-        let ig: i64 = (g * 256.0) as i64;
-        let ib: i64 = (b * 256.0) as i64;
+        let ir: u8 = (r * 256.0) as u8;
+        let ig: u8 = (g * 256.0) as u8;
+        let ib: u8 = (b * 256.0) as u8;
 
-        output.push_str(format!("{} {} {}\n", ir, ig, ib).as_str());
+        img.put_pixel(x, y, Rgb([ir, ig, ib]));
     }
 
-    use std::fs::write;
-    write("docs/testimage.ppm", output).expect("Unable to write to file.");
+    img.save("docs/testimage.png")
+        .expect("Failed to save as png.");
 
     Ok(())
 }
