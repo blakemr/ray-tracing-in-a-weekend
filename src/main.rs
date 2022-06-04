175d8b9d181ec86011d1101a7c1c69b6eb6e7da5
Matthew <matthewblake0625@gmail.com>
2022-06-04 10:25:36 -0400

    cleaned up main a bit.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use camera::Camera;
 use materials::{Dielectric, Diffuse, Metal};
-use ray_tracing_in_a_weekend::{utilities::Color, *};
+use ray_tracing_in_a_weekend::{materials::Material, utilities::Color, *};
 use render::Render;
 use scene::Scene;
 use std::rc::Rc;
@@ -14,12 +14,10 @@ fn main() {
     const BOUNCES: u64 = 10;
 
     let mut scene = Scene::new();
-    let mat_ground = Rc::new(Diffuse::new(Color::new(0.8, 0.8, 0.0)));
-    let mat_center = Rc::new(Dielectric::new(1.5));
-    let mat_left = Rc::new(Dielectric::new(1.5));
-    let mat_diffuse_pink = Rc::new(Diffuse::new(Color::new(0.7, 0.3, 0.3)));
-    let mat_metal_low_fuzz = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
-    let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
+    let mat_ground = get_mat(Materials::DiffuseYellow);
+    let mat_center = get_mat(Materials::DielectricClear);
+    let mat_left = get_mat(Materials::DielectricClear);
+    let mat_right = get_mat(Materials::MetalHighFuzz);
 
     scene.add_sphere(0.0, 0.0, -1.0, 0.5, mat_center);
     scene.add_sphere(0.0, -100.5, -1.0, 100.0, mat_ground);
@@ -31,4 +29,22 @@ fn main() {
     let mut img = Render::new(IMAGE_WIDTH, IMAGE_HEIGHT, SAMPLES_PP, BOUNCES);
     img.render(&camera, &scene);
     img.save("docs/testimage.png");
+}
+
+enum Materials {
+    DiffuseYellow,
+    DielectricClear,
+    DiffusePink,
+    MetalLowFuzz,
+    MetalHighFuzz,
+}
+
+fn get_mat(mat: Materials) -> Rc<dyn Material> {
+    match mat {
+        Materials::DiffuseYellow => Rc::new(Diffuse::new(Color::new(0.8, 0.8, 0.0))),
+        Materials::DiffusePink => Rc::new(Diffuse::new(Color::new(0.7, 0.3, 0.3))),
+        Materials::MetalLowFuzz => Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3)),
+        Materials::MetalHighFuzz => Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0)),
+        Materials::DielectricClear => Rc::new(Dielectric::new(1.5)),
+    }
 }
